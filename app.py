20feb643407bc44d27acc924970717d0fadeb386
gopisuresh236-afk <gopisuresh236@gmail.com>
2025-11-14 08:19:02 +0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ button:hover { background: #025aa5; }
 </style>
 </head>
 <body>
-
+<img src="logo.png" alt="AI Logo" width="100" height="100">
 <h2> Dr Kumaravel Govindharasu AI  (UTAS - Ibri)</h2>
 
 <div class="chat" id="chat">{{chat|safe}}</div>
