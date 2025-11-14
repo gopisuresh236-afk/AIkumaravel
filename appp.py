fc8ffb81311044a3631ca890986c417643d7e770
gopisuresh236-afk <gopisuresh236@gmail.com>
2025-11-14 11:04:30 +0400

    Update appp.py

--- appp.py
+++ appp.py
@@ -36,7 +36,7 @@ h2 { display: flex; align-items: center; gap: 10px; }
 
 <h2>
    
-       Dr Kumaravel Govindharasu - KumBot AI (UTAS, ibri, Sultanate of Oman)
+       Dr Kumaravel Govindharasu - KuBo AI (UTAS, ibri, Sultanate of Oman)
 </h2>
 
 <div class="chat" id="chat">{{chat|safe}}</div>
@@ -131,4 +131,5 @@ def home():
 # ------------------- RUN APP -------------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
